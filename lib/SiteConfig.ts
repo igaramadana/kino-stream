ec8a8ee0badeca7523fa5e54919f010ddaa4f145
igaramadana <igrmdns085@gmail.com>
2026-05-09 11:01:06 +0700

    feat: add site config

---
+++ lib/SiteConfig.ts
@@ -0,0 +1,7 @@
+export const SiteConfig = {
+  Nama: "Kino Stream",
+  Deskripsi:
+    "Mini project movie dan series list menggunakan Next.js, TypeScript, Tailwind CSS, Bun, dan TMDB API.",
+  Aksen: "#8FDC2C",
+  Github: "https://github.com/username/dipa-movie-list"
+};
