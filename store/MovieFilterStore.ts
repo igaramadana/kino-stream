6029328c3c812178e8e210dccd1ffc5bb1439152
igaramadana <igrmdns085@gmail.com>
2026-05-09 13:36:42 +0700

    feat: add default movie filter state

---
+++ store/MovieFilterStore.ts
@@ -0,0 +1,7 @@
+import type { FilterMovieState } from "@/types/Filter";
+
+export const FilterMovieAwal: FilterMovieState = {
+    kataKunci: "",
+    tipe: "all",
+    urutan: "rating-tertinggi"
+};
