1fc8f29b8bde0b5e5adcb177d497ffff2eaa2177
igaramadana <igrmdns085@gmail.com>
2026-05-09 11:04:41 +0700

    fix: update GitHub link in SiteConfig

--- lib/SiteConfig.ts
+++ lib/SiteConfig.ts
@@ -3,5 +3,5 @@ export const SiteConfig = {
   Deskripsi:
     "Mini project movie dan series list menggunakan Next.js, TypeScript, Tailwind CSS, Bun, dan TMDB API.",
   Aksen: "#8FDC2C",
-  Github: "https://github.com/username/dipa-movie-list"
+  Github: "https://github.com/igaramadana/kino-stream"
 };
